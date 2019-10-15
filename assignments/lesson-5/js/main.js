3baed72e59ff95267c9c0f7d225016b8fa338883
harlee23 <harleenelson23@gmail.com>
2019-10-15 14:31:06 -0500

    save

---
+++ assignments/lesson-5/js/main.js
@@ -0,0 +1,12 @@
+const options =
+    {weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'};
+document.getElementById('lastUpdated').textContent = new Date(document.lastModified).toLocaleDateString('en-US',options);
+document.getElementById('currentDate').textContent = new Date(document.lastModified).getFullYear();
+
+function toggleMenu() {
+
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
