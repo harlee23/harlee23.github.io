4e88f6fbebbf98488193fb8450257c10ca9cdef2
harlee23 <harleenelson23@gmail.com>
2019-10-25 21:48:13 -0500

    save

--- assignments/lesson-6/js/windchill.js
+++ assignments/lesson-6/js/windchill.js
@@ -3,11 +3,11 @@ function windchill(){
     var windspeed = parseFloat(document.getElementById("windspeed").innerHTML);
 
     if (temperature>=50 || windspeed<=3){
-        document.getElementsById("windchill").innerHTML="N/A";
+        document.getElementById("windchill").innerHTML="N/A";
     }
     else{
         var windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16));
-        document.getElementsById("windchill").innerHTML=windchill.toFixed(2);
+        document.getElementById("windchill").innerHTML=windchill.toFixed(0);
     }
 }
 
