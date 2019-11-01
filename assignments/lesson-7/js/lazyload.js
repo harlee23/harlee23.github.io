34641202519fb9084c47486ca7b8a89fd8937ac6
harlee23 <harleenelson23@gmail.com>
2019-10-31 20:57:23 -0500

    save

--- assignments/lesson-7/js/lazyload.js
+++ assignments/lesson-7/js/lazyload.js
@@ -1,57 +1,30 @@
-function preloadImage(img)
-{
+function preloadImage(img){
 
     const src = img.getAttribute("data-src");
-
-    if(!src)
-    {
-
-        return;
-
-    }
-
+    if(!src){
+    return;
+}
     img.src = src;
-
 }
 
-const images = document.querySelectorAll("[data-src]");
-
-const imgOptions = {
-
-    threshhold: 0,
-
-    rootMargin: "0px 0px 300px 0px"
-
+    const images = document.querySelectorAll("[data-src]");
+    const imgOptions = {
+        threshhold: 0,
+        rootMargin: "0px 0px 300px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) =>
-{
-
-    entries.forEach(entry =>
-    {
-        
-        if (!entry.isIntersecting)
-        {
-
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach(entry => {
+        if (!entry.isIntersecting){
             return;
-
-        }
-        
-        else
-        {
-
+}
+        else{
             preloadImage(entry.target);
             imgObserver.unobserve(entry.target);
-
-        }
-
-    });
-
-}, imgOptions);
-
-images.forEach(image =>
-{
-
-    imgObserver.observe(image);
-
+}
+});
+}, 
+        imgOptions);
+        images.forEach(image =>{
+        imgObserver.observe(image);
 });
